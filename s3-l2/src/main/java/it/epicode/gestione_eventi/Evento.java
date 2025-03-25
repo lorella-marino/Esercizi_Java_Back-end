35ef108b69fb6c14b4b1ea173cf3a14553441236
lorella-marino <lorellamarino@gmail.com>
2025-03-25 23:17:32 +0100

    commit 25/03

---
+++ s3-l2/src/main/java/it/epicode/gestione_eventi/Evento.java
@@ -0,0 +1,43 @@
+package it.epicode.gestione_eventi;
+
+import jakarta.persistence.*;
+import jakarta.persistence.GenerationType;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table (name = "evento")
+public class Evento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column (length = 100, nullable = false)
+    private String titolo;
+
+    @Column (nullable = false)
+    private LocalDate dataEvento;
+
+    @Column (length = 200, nullable = false)
+    private String descrizione;
+
+    @Enumerated(EnumType.STRING)
+    private TipoEvento tipoEvento;
+
+    @Column
+    private int numeroMassimoPartecipanti;
+
+    public Evento( String titolo, LocalDate dataEvento, String descrizione, TipoEvento tipoEvento, int numeroMassimoPartecipanti) {
+        this.titolo = titolo;
+        this.dataEvento = dataEvento;
+        this.descrizione = descrizione;
+        this.tipoEvento = tipoEvento;
+        this.numeroMassimoPartecipanti = numeroMassimoPartecipanti;
+    }
+
+    public Evento getEvento() {
+        return this;
+    }
+}
---
+++ s3-l2/src/main/java/it/epicode/gestione_eventi/Main.java
@@ -0,0 +1,41 @@
+package it.epicode.gestione_eventi;
+
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("epicode");
+    EntityManager em = emf.createEntityManager();
+
+    EventoDAO eventoDAO = new EventoDAO(em);
+
+    eventoDAO.save(new Evento( "Evento1", LocalDate.now(), "Descrizione1", TipoEvento.PUBBLICO, 50));
+    eventoDAO.save(new Evento( "Evento2", LocalDate.now(), "Descrizione2", TipoEvento.PRIVATO, 110));
+    eventoDAO.save(new Evento( "Evento3", LocalDate.now(), "Descrizione3", TipoEvento.PUBBLICO, 120));
+    eventoDAO.save(new Evento( "Evento4", LocalDate.now(), "Descrizione4", TipoEvento.PRIVATO, 85));
+
+    // altro metodo in cui poter salvare
+/*  Evento evento = new Evento("Evento1", LocalDate.now(), "Descrizione1", TipoEvento.PUBBLICO, 50);
+    Evento evento2 = new Evento("Evento2", LocalDate.now(), "Descrizione2", TipoEvento.PRIVATO, 110);
+    Evento evento3 = new Evento("Evento3", LocalDate.now(), "Descrizione3", TipoEvento.PUBBLICO, 120);
+    Evento evento4 = new Evento("Evento4", LocalDate.now(), "Descrizione4", TipoEvento.PRIVATO, 85);
+
+    eventoDAO.save(evento);
+    eventoDAO.save(evento2);
+    eventoDAO.save(evento3);
+    eventoDAO.save(evento4); */
+
+    eventoDAO.getById(1L);
+
+    eventoDAO.delete( eventoDAO.getById(4L) );
+
+    em.close();
+    emf.close();
+}
+
+}

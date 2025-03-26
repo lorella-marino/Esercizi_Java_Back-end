d62da08f5ca8af838df5702ed81c486f7eae3915
lorella-marino <lorellamarino@gmail.com>
2025-03-26 15:30:10 +0100

    commit 25/03

--- s3-l2/src/main/java/it/epicode/gestione_eventi/Main.java
+++ s3-l2/src/main/java/it/epicode/gestione_eventi/Main.java
@@ -1,6 +1,9 @@
 package it.epicode.gestione_eventi;
 
 
+import it.epicode.gestione_eventi.evento.Evento;
+import it.epicode.gestione_eventi.evento.EventoDAO;
+import it.epicode.gestione_eventi.evento.TipoEvento;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
@@ -14,25 +17,23 @@ public class Main {
 
     EventoDAO eventoDAO = new EventoDAO(em);
 
-    eventoDAO.save(new Evento( "Evento1", LocalDate.now(), "Descrizione1", TipoEvento.PUBBLICO, 50));
-    eventoDAO.save(new Evento( "Evento2", LocalDate.now(), "Descrizione2", TipoEvento.PRIVATO, 110));
-    eventoDAO.save(new Evento( "Evento3", LocalDate.now(), "Descrizione3", TipoEvento.PUBBLICO, 120));
-    eventoDAO.save(new Evento( "Evento4", LocalDate.now(), "Descrizione4", TipoEvento.PRIVATO, 85));
-
-    // altro metodo in cui poter salvare
-/*  Evento evento = new Evento("Evento1", LocalDate.now(), "Descrizione1", TipoEvento.PUBBLICO, 50);
+    Evento evento = new Evento("Evento1", LocalDate.now(), "Descrizione1", TipoEvento.PUBBLICO, 50);
     Evento evento2 = new Evento("Evento2", LocalDate.now(), "Descrizione2", TipoEvento.PRIVATO, 110);
     Evento evento3 = new Evento("Evento3", LocalDate.now(), "Descrizione3", TipoEvento.PUBBLICO, 120);
     Evento evento4 = new Evento("Evento4", LocalDate.now(), "Descrizione4", TipoEvento.PRIVATO, 85);
 
-    eventoDAO.save(evento);
-    eventoDAO.save(evento2);
-    eventoDAO.save(evento3);
-    eventoDAO.save(evento4); */
+    em.getTransaction().begin();
 
-    eventoDAO.getById(1L);
+    eventoDAO.insert(evento);
+    eventoDAO.insert(evento2);
+    eventoDAO.insert(evento3);
+    eventoDAO.insert(evento4);
 
-    eventoDAO.delete( eventoDAO.getById(4L) );
+    eventoDAO.find(1L);
+
+    eventoDAO.delete( 4L );
+
+    em.getTransaction().commit();
 
     em.close();
     emf.close();
--- s3-l2/src/main/java/it/epicode/gestione_eventi/TipoEvento.java
+++
@@ -1,5 +0,0 @@
-package it.epicode.gestione_eventi;
-
-public enum TipoEvento {
-    PUBBLICO, PRIVATO
-}
